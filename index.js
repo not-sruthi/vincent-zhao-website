79ac5878f36d39055cfc14a72a8e24b43ba11437
not-sruthi <75844345+not-sruthi@users.noreply.github.com>
2021-03-27 18:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,16 +18,7 @@ function readResults(year){
     results.on('value', (data) => {
      var ranks = data.val(); 
      var finalranks=ranks.toString().split(',');
-     document.getElementById("result1").innerHTML ="<br > 1. " +finalranks[1];
-     document.getElementById("result2").innerHTML ="<br > 2. " +finalranks[2];
-     document.getElementById("result3").innerHTML ="<br > 3. " +finalranks[3];
-     document.getElementById("result4").innerHTML ="<br > 4. " +finalranks[4];
-     document.getElementById("result5").innerHTML ="<br > 5. " +finalranks[5];
-     document.getElementById("result6").innerHTML ="<br > 6. " +finalranks[6];
-     document.getElementById("result7").innerHTML ="<br > 7. " +finalranks[7];
-     document.getElementById("result8").innerHTML ="<br > 8. " +finalranks[8];
-
-    
+     document.getElementById("result1").innerHTML ="<br > 1. " +finalranks[1];    
 })
 }
 
